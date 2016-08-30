9c8c71a370a3c1180dca28c5ea045d36e85bbf7d
Robert Chubb <paraboliclabs@gmail.com>
2016-08-30 02:01:16 -0400

    7

---
+++ Source/MachinaPlays/Machina.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MachinaPlays.h"
+#include "Machina.h"
+
+Machina::Machina()
+{
+}
+
+Machina::~Machina()
+{
+}
